b4d9734204cb68ba322a9429a31dce0b71680da6
Andrew Stewart <andrew@stwrt.ca>
2016-09-24 21:48:36 -0700

    Simplify test_helper.js

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,16 +2,6 @@
 
 process.env.NODE_ENV = "test";
 
-require("mocha");
-
-const path = require("path");
-const chai = require("chai");
+global.chai = require("chai");
 
 require("source-map-support/register");
-
-function src(dest) {
-  return require(path.join(__dirname, "..", "src", dest));
-}
-
-global.chai = chai;
-global.src = src;
